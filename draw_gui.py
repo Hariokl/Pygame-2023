ca14b5dca825d7929d8d5ab69379380ce160b9fe
Hariokl <sevabu06@gmail.com>
2023-03-22 21:37:25 +0300

    some gui improvements

--- draw_gui.py
+++ draw_gui.py
@@ -8,20 +8,6 @@ def setup():
     diff_bar_dict = {"width": st.WIDTH * 2 // 7, "height": st.HEIGHT / 12, "pos": (st.WIDTH * 2 // 3, st.HEIGHT // 15)}
 
 
-def diff_bar():
-    scr = pg.Surface((diff_bar_dict["width"], diff_bar_dict["height"]))
-    rect = scr.get_rect()
-    gradient_rect(scr, (0, 255, 0), (255, 0, 0), rect)
-    pg.draw.rect(scr, (0, 0, 0), scr.get_rect(), 4)
-    st.gui_display.blit(scr, (diff_bar_dict["pos"][0], diff_bar_dict["pos"][1]))
-
-
-def diff_bar_update(scr):
-    x = Level.level.time / 200  # 200 - time to get the hardest difficulty
-    pg.draw.rect(scr, (0, 0, 0), (diff_bar_dict["pos"][0] + diff_bar_dict["width"] * x, diff_bar_dict["pos"][1],
-                                  diff_bar_dict["width"] * (1 - x), diff_bar_dict["height"]), 0)
-
-
 def update():
     scr = st.gui_display.copy()
 
@@ -37,3 +23,17 @@ def gradient_rect(window, left_colour, right_colour, target_rect):
     pg.draw.line(colour_rect, right_colour, (1, 0), (1, 1))  # right colour line
     colour_rect = pg.transform.smoothscale(colour_rect, (target_rect.width, target_rect.height))  # stretch!
     window.blit(colour_rect, target_rect)
+
+
+def diff_bar():
+    scr = pg.Surface((diff_bar_dict["width"], diff_bar_dict["height"]))
+    rect = scr.get_rect()
+    gradient_rect(scr, (0, 255, 0), (255, 0, 0), rect)
+    pg.draw.rect(scr, (0, 0, 0), scr.get_rect(), 4)
+    st.gui_display.blit(scr, (diff_bar_dict["pos"][0], diff_bar_dict["pos"][1]))
+
+
+def diff_bar_update(scr):
+    x = Level.level.time / 200  # 200 - time to get the hardest difficulty
+    pg.draw.rect(scr, (0, 0, 0), (diff_bar_dict["pos"][0] + diff_bar_dict["width"] * x, diff_bar_dict["pos"][1],
+                                  diff_bar_dict["width"] * (1 - x), diff_bar_dict["height"]), 0)
